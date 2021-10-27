5e849e73e71f78fea10e8ba551072e2e8d42b88b
Nibby Nebbulous <nibbula@gmail.com>
2021-10-26 18:09:20 -0700

    docstring edit

--- package.lisp
+++ package.lisp
@@ -4,18 +4,17 @@
 
 (defpackage :lish
   (:documentation
-   "Unix Shell & Lisp somehow smushed together.
+   "Lish is both a command shell and a Lisp REPL.
 
-Lish is a program designed to make typing both operating system commands, and
-Common Lisp expressions, convienient. It combines the features of a
-traditional operating system shell with a Lisp REPL. It's designed to
-hopefully have little annoyance to people familair with a POSIX shell. But it
-does not have exact compatibility with POSIX shells.
+Lish is designed to make typing both operating system commands and Common Lisp
+expressions convienient. It combines the features of a traditional operating
+system shell with a Lisp REPL. It's designed to hopefully have little annoyance
+to people familair with a POSIX shell. But it does not have exact compatibility
+with POSIX shells.
 
 The motivation for writing Lish came from the annoyance of having to swtich
 between a Lisp REPL and a Unix shell. Lish may be used as a command shell,
-without any particular knowledge of it's Lisp programming features.
-")
+without any particular knowledge of it's Lisp programming features.")
   (:use :cl :dlib :opsys :dlib-misc :stretchy :char-util :glob
 	:table :table-print :reader-ext :completion :keymap
 	:terminal :terminal-ansi :rl :fatchar :fatchar-io :collections
